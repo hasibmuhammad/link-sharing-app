21fa5e84502eed2a4ee496d268d7f7cd62e1fa4c
hasibmuhammad <hasibmu99@gmail.com>
2024-10-22 21:13:27 +0600

    modified the responsive of preview page

--- src/app/components/ui/PreviewHeader/index.tsx
+++ src/app/components/ui/PreviewHeader/index.tsx
@@ -32,7 +32,7 @@ const PreviewHeader = ({ onCopy }: IProps): JSX.Element => {
         <section>
             <div className="flex justify-between">
                 <Link href={"/profile"}>
-                    <button className="capitalize text-primary border border-primary rounded-lg px-5 py-2 hover:bg-primary/10 font-semibold">Back to editor</button>
+                    <button className="capitalize text-primary border border-primary rounded-lg px-3 md:px-5 py-2 hover:bg-primary/10 font-semibold">Back to editor</button>
                 </Link>
                 <button onClick={handleCopytoClipboard} className="bg-primary capitalize  border border-primary rounded-lg px-5 py-2 hover:bg-primary/60 font-semibold text-white">Share Link</button>
             </div>
